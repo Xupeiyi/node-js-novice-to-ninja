63cb39754d07f06128b9a13ef5d50cf88bc9f5a3
Peiyi Xu <xu_py311@outlook.com>
2023-07-24 23:03:20 -0400

    added asset and middleware

--- chapter5/express-example/index.js
+++ chapter5/express-example/index.js
@@ -1,22 +1,44 @@
 // Express Application
 import express from "express";
 
+import { fileURLToPath } from 'url';
+import { dirname, sep } from 'path';
+
+
 // configuration
-const cfg = {
-    port: process.env.PORT || 3000
-};
+const
+    __dirname = dirname(fileURLToPath( import.meta.url )) + sep,
+    cfg = {
+        port: process.env.PORT || 3000,
+        dir: {
+            root: __dirname,
+            static: __dirname + 'static' + sep
+        }
+    };
+
+
+console.dir(cfg, { depth: null, color: true });
 
 // initiation
 const app = express();
+
+// log every request to the terminal
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+  });
 
 // home page route
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// serve static assets
+app.use(express.static(cfg.dir.static));
+
 // start server
 app.listen(cfg.port, () => {
     console.log(`Example app listening at http://localhost:${cfg.port}`);
 })
 
-
+export { cfg, app };
