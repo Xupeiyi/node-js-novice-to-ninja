cf861f1c90c09b41547e588ef2cfbb44b7a0fbdd
Peiyi Xu <xu_py311@outlook.com>
2023-07-24 23:14:37 -0400

    added more middleware

--- chapter5/express-example/index.js
+++ chapter5/express-example/index.js
@@ -1,6 +1,6 @@
 // Express Application
-import express from "express";
-
+import express from 'express';
+import compression from 'compression';
 import { fileURLToPath } from 'url';
 import { dirname, sep } from 'path';
 
@@ -22,11 +22,24 @@ console.dir(cfg, { depth: null, color: true });
 // initiation
 const app = express();
 
+// do not identify Express
+app.disable('x-powered-by');
+
+
+// HTTP compression
+app.use(compression());
+
 // log every request to the terminal
 app.use((req, res, next) => {
     console.log(req.url);
     next();
   });
+
+// 404 error
+app.use((req, res) => {
+    res.status(404).send('Not found');
+});
+
 
 // home page route
 app.get('/', (req, res) => {
